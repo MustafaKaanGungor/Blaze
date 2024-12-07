9f0392c306ede2cdee6444b334d449f3267267be
Mustafa Kaan Güngör <kaan062004+github@gmail.com>
2024-12-07 12:50:53 +0300

    small changes to UI

--- Blaze/Form1.Designer.cs
+++ Blaze/Form1.Designer.cs
@@ -41,6 +41,9 @@
             this.keepLoginCheckbox = new System.Windows.Forms.CheckBox();
             this.signUpButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -52,7 +55,8 @@
             this.Blaze.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
             this.Blaze.Font = new System.Drawing.Font("Impact", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Blaze.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.Blaze.Location = new System.Drawing.Point(1, 5);
+            this.Blaze.Location = new System.Drawing.Point(0, 0);
+            this.Blaze.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Blaze.Name = "Blaze";
             this.Blaze.Size = new System.Drawing.Size(75, 35);
             this.Blaze.TabIndex = 2;
@@ -66,9 +70,10 @@
             this.iconSizeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.iconSizeButton.FlatAppearance.BorderSize = 0;
             this.iconSizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.iconSizeButton.Location = new System.Drawing.Point(1332, 7);
+            this.iconSizeButton.Location = new System.Drawing.Point(1249, 8);
+            this.iconSizeButton.Margin = new System.Windows.Forms.Padding(2);
             this.iconSizeButton.Name = "iconSizeButton";
-            this.iconSizeButton.Size = new System.Drawing.Size(25, 25);
+            this.iconSizeButton.Size = new System.Drawing.Size(24, 25);
             this.iconSizeButton.TabIndex = 5;
             this.iconSizeButton.UseVisualStyleBackColor = false;
             this.iconSizeButton.Click += new System.EventHandler(this.button3_Click);
@@ -80,9 +85,10 @@
             this.closeAppButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.closeAppButton.FlatAppearance.BorderSize = 0;
             this.closeAppButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.closeAppButton.Location = new System.Drawing.Point(1413, 7);
+            this.closeAppButton.Location = new System.Drawing.Point(1325, 8);
+            this.closeAppButton.Margin = new System.Windows.Forms.Padding(2);
             this.closeAppButton.Name = "closeAppButton";
-            this.closeAppButton.Size = new System.Drawing.Size(25, 25);
+            this.closeAppButton.Size = new System.Drawing.Size(24, 25);
             this.closeAppButton.TabIndex = 4;
             this.closeAppButton.UseVisualStyleBackColor = false;
             this.closeAppButton.Click += new System.EventHandler(this.button2_Click);
@@ -94,9 +100,10 @@
             this.resizeAppButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.resizeAppButton.FlatAppearance.BorderSize = 0;
             this.resizeAppButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.resizeAppButton.Location = new System.Drawing.Point(1373, 7);
+            this.resizeAppButton.Location = new System.Drawing.Point(1288, 8);
+            this.resizeAppButton.Margin = new System.Windows.Forms.Padding(2);
             this.resizeAppButton.Name = "resizeAppButton";
-            this.resizeAppButton.Size = new System.Drawing.Size(25, 25);
+            this.resizeAppButton.Size = new System.Drawing.Size(24, 25);
             this.resizeAppButton.TabIndex = 3;
             this.resizeAppButton.UseVisualStyleBackColor = false;
             this.resizeAppButton.Click += new System.EventHandler(this.button1_Click);
@@ -104,18 +111,20 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
-            this.pictureBox1.Location = new System.Drawing.Point(-12, -17);
+            this.pictureBox1.Location = new System.Drawing.Point(-11, -18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1466, 57);
+            this.pictureBox1.Size = new System.Drawing.Size(1386, 61);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(566, 38);
+            this.pictureBox2.Location = new System.Drawing.Point(517, 47);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(888, 777);
+            this.pictureBox2.Size = new System.Drawing.Size(832, 732);
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
             // 
@@ -123,8 +132,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label1.Location = new System.Drawing.Point(21, 63);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(90)))), ((int)(((byte)(30)))));
+            this.label1.Location = new System.Drawing.Point(20, 64);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(500, 57);
             this.label1.TabIndex = 7;
@@ -132,65 +142,137 @@
             // 
             // mailTextBox
             // 
-            this.mailTextBox.Location = new System.Drawing.Point(43, 251);
+            this.mailTextBox.AccessibleName = "";
+            this.mailTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(49)))));
+            this.mailTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mailTextBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.mailTextBox.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mailTextBox.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.mailTextBox.Location = new System.Drawing.Point(40, 221);
+            this.mailTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.mailTextBox.Name = "mailTextBox";
-            this.mailTextBox.Size = new System.Drawing.Size(315, 22);
+            this.mailTextBox.Size = new System.Drawing.Size(363, 38);
             this.mailTextBox.TabIndex = 8;
-            this.mailTextBox.Text = "e mail";
+            this.mailTextBox.TextChanged += new System.EventHandler(this.mailTextBox_TextChanged);
             // 
             // passwordTextBox
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(43, 314);
+            this.passwordTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(49)))));
+            this.passwordTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordTextBox.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.passwordTextBox.Location = new System.Drawing.Point(40, 319);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(161, 22);
+            this.passwordTextBox.Size = new System.Drawing.Size(363, 38);
             this.passwordTextBox.TabIndex = 9;
-            this.passwordTextBox.Text = "password";
             // 
             // loginButton
             // 
-            this.loginButton.Location = new System.Drawing.Point(43, 429);
+            this.loginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(90)))), ((int)(((byte)(30)))));
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.loginButton.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.loginButton.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.loginButton.Location = new System.Drawing.Point(40, 454);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(2);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(128, 42);
+            this.loginButton.Size = new System.Drawing.Size(130, 44);
             this.loginButton.TabIndex = 10;
             this.loginButton.Text = "Login";
-            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
             // 
             // keepLoginCheckbox
             // 
             this.keepLoginCheckbox.AutoSize = true;
+            this.keepLoginCheckbox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.keepLoginCheckbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.keepLoginCheckbox.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.keepLoginCheckbox.Location = new System.Drawing.Point(43, 368);
+            this.keepLoginCheckbox.Location = new System.Drawing.Point(40, 424);
+            this.keepLoginCheckbox.Margin = new System.Windows.Forms.Padding(2);
             this.keepLoginCheckbox.Name = "keepLoginCheckbox";
-            this.keepLoginCheckbox.Size = new System.Drawing.Size(142, 20);
+            this.keepLoginCheckbox.Size = new System.Drawing.Size(165, 24);
             this.keepLoginCheckbox.TabIndex = 11;
             this.keepLoginCheckbox.Text = "Keep me logged in";
             this.keepLoginCheckbox.UseVisualStyleBackColor = true;
             // 
             // signUpButton
             // 
-            this.signUpButton.Location = new System.Drawing.Point(43, 577);
+            this.signUpButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(90)))), ((int)(((byte)(30)))));
+            this.signUpButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.signUpButton.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.signUpButton.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.signUpButton.Location = new System.Drawing.Point(40, 550);
+            this.signUpButton.Margin = new System.Windows.Forms.Padding(2);
             this.signUpButton.Name = "signUpButton";
-            this.signUpButton.Size = new System.Drawing.Size(91, 32);
+            this.signUpButton.Size = new System.Drawing.Size(130, 44);
             this.signUpButton.TabIndex = 12;
             this.signUpButton.Text = "Sign Up";
-            this.signUpButton.UseVisualStyleBackColor = true;
+            this.signUpButton.UseVisualStyleBackColor = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label2.Location = new System.Drawing.Point(40, 547);
+            this.label2.Location = new System.Drawing.Point(38, 515);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(136, 16);
+            this.label2.Size = new System.Drawing.Size(211, 23);
             this.label2.TabIndex = 13;
             this.label2.Text = "Dont have a account?";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(36, 185);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 23);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "E-mail or username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Location = new System.Drawing.Point(36, 284);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Password";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Lucida Sans Unicode", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(31, 364);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 35);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Forgot your password?";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
-            this.ClientSize = new System.Drawing.Size(1450, 800);
+            this.ClientSize = new System.Drawing.Size(1360, 812);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.signUpButton);
             this.Controls.Add(this.keepLoginCheckbox);
@@ -205,6 +287,7 @@
             this.Controls.Add(this.Blaze);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -228,6 +311,9 @@
         private System.Windows.Forms.CheckBox keepLoginCheckbox;
         private System.Windows.Forms.Button signUpButton;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Blaze/Form1.cs
+++ Blaze/Form1.cs
@@ -46,5 +46,30 @@ namespace Blaze
         {
 
         }
+
+        private void mailTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
